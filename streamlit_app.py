a32028ef922237743163748a37124bf15c117434
GuLF-AI <gulf.ai.info@gmail.com>
2025-05-17 04:07:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ if __name__=='__main__':
     #comtypes.CoInitialize()
     infolder=os.path.dirname(os.path.abspath(__file__))
     #path = os.path.abspath("..\\Input\\")
-    path = "./input/"
+    path = "pdf_bot/input/"
     files = os.listdir(path)
     
         
