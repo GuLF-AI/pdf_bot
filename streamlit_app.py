2fafeb56f76b41ccf95c0c5ede05e5c3c6332dab
GuLF-AI <gulf.ai.info@gmail.com>
2025-05-17 04:02:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ if __name__=='__main__':
     
     #comtypes.CoInitialize()
     infolder=os.path.dirname(os.path.abspath(__file__))
-    path = os.path.abspath(__file__+"..\\..\\Input\\")
+    path = os.path.abspath("\\Input\\")
     files = os.listdir(path)
     
         
